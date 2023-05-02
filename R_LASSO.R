7eca6b71fff48ff9e13a1b8daf83d351115c8d18
gdwinnell <77944419+gdwinnell@users.noreply.github.com>
2023-05-02 15:05:28 -0400

    R_LASSO.R

--- R_LASSO.R
+++ R_LASSO.R
@@ -1,3 +1,4 @@
+data(mtcars)
 head(mtcars)
 summary(mtcars)
 
@@ -20,14 +21,14 @@ best_lambda
 
 
 #plot of test MSE
-plot(cv_model)
+plot(cv_model) 
 
 #coefficients of best model
-best_model <- glmnet(x, y, alpha = 1, lambda = best_lambda)
+best_model <- glmnet(x, y, alpha = 0, lambda = best_lambda)
 coef(best_model)
 
 #new observation
-new = matrix(c(24, 2.5, 3.5, 18.5), nrow=1, ncol=4)
+new = matrix(c(24, 2.5, 3.5, 18.5), nrow=1, ncol=4) 
 
 #lasso regression model to predict response value
 predict(best_model, s = best_lambda, newx = new)
